657d797ce29523b9eee5d2fc3d6470dbc489e790
Sudarshan <jhengis@protonmail.com>
2023-08-30 17:30:38 +0530

    created load balancer

---
+++ load-balancer/variables.tf
@@ -0,0 +1,8 @@
+# ALB variables
+variable "project_name" {}
+variable "environment" {}
+variable "alb_security_group_id" {}
+variable "public_subnet_az1_id" {}
+variable "public_subnet_az2_id" {}
+variable "target_type" {}
+variable "vpc_id" {}
